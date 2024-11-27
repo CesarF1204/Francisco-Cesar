e57c081a73db37e2882b7b8eb4a1f28a250500a8
Cesar Francisco <cfrancisco@village88.com>
2024-11-28 02:05:51 +0800

    updated files for d13-14 and d15-16 folder

---
+++ D13-14/demo/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const todoRoutes = require('./routes/todoRoutes');
+require('dotenv').config();
+
+// Express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+const connectToDatabase = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
+    }
+    catch(error){
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+// Routes
+app.use('/todos', todoRoutes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+    connectToDatabase();
+});
