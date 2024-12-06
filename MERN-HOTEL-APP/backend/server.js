42034f9a9642d2b3007e6665a5759094f38734c5
Cesar Francisco <princexcesar@gmail.com>
2024-12-07 03:25:03 +0800

    added mern-hotel-app log-in and registration feature

---
+++ MERN-HOTEL-APP/backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
+
+// Test endpoint
+// app.get("/api/test", async (req, res) => {
+//     res.json({ message: "hello from express endpoint" });
+// });
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
